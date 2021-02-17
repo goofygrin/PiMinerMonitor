7906e797c49278f74ed83e6604060a1c6dec033f
Sean Goodpasture <goofygrin@gmail.com>
2021-02-16 22:58:23 -0700

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,94 @@
 # PiMinerMonitor
+Monitor your mining rig using a Raspberry Pi!
+
+This is based on [this excellent guide from Adafruit](https://learn.adafruit.com/pi-hole-ad-blocker-with-pi-zero-w) for setting up a Raspberry Pi as a pihole ad blocking service.
+
+Currently the code supports Ethermine, but Flexpool should be pretty easy to setup.
+
+Once it's running it will show the current unpaid balance, the USD value of that balance, and the USD-ETH value.  If the number of workers is <2 or the number of invalid shares is >= 3 then the unpaid balance will be red.
+
+By pressing the top button of the display for a second, it will toggle to a more detailed view with the hash rate, number of workers, the stale and invalid share counts. Holding the button again will toggle back to the simpler display.
+
+Both displays will show a 5 minute countdown to the next refresh.
+
+## Parts
+1. [Raspberry Pi](https://www.microcenter.com/product/502843/raspberry-pi-zero-wh---with-pre-soldered-headers) - I used a Raspberry Pi Zero WH (H means it has the headers pre-soldered on) - $11.99 @ Microcenter
+2. [Adafruit Mini PiTFT](https://www.adafruit.com/product/4393) - $14.99 @ Microcenter
+3. MicroSD card
+4. Power Supply for the pi
+
+## Setup
+
+Install the PiTFT onto the pi.
+
+Follow the instructions [here](https://www.raspberrypi.org/software/) to install Raspberry Pi OS Lite on the SD card.
+
+Once the bare OS is copied to the card, plug it back into your computer and prep it for headless running (you can follow the [official instructions](https://www.raspberrypi.org/documentation/configuration/wireless/headless.md) if you'd like):
+- add a blank file named ssh to the card to turn on ssh by default
+- add a file named wpa_supplicant.conf with the following content (updating to replace the three settings with your info):
+  ```
+  ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+  update_config=1
+  country=<Insert 2 letter ISO 3166-1 country code here>
+
+  network={
+    ssid="<Name of your wireless LAN>"
+    psk="<Password for your wireless LAN>"
+  }
+  ```
+
+Insert the SD card into the pi and power it on.  With a pi zero the little green led will flicker as it boots.
+
+Look in your wireless router's DHCP list to find the IP address of the pi.
+
+Launch your favorite SSH program (PuTTY on windows or ssh in a shell on OSX/linux) and ssh into the pi.  The default login is pi/raspberry.
+
+Run the following commands:
+1. Update your Pi
+   ```
+   sudo apt-get update
+   sudo apt-get upgrade
+   ```
+2. Install pip
+   ```
+   sudo apt-get install python3-pip   
+   ```
+3. Install setup tools
+   ```
+   sudo pip3 install --upgrade setuptools
+   ```
+4. Install the Adafruit tools
+   ```
+   sudo pip3 install --upgrade adafruit-python-shell
+   ```
+5. Get the setup script and run it (this sets up a number of things and turns on SPI to talk to the display)
+   ```
+   wget https://raw.githubusercontent.com/adafruit/Raspberry-Pi-Installer-Scripts/master/raspi-blinka.py
+   sudo python3 raspi-blinka.py
+   ```
+6. Say yes to setting Python3 as the default
+7. When prompted to reboot, select yes
+8. Once the pi is rebooted, ssh back into it
+9. Download the script
+   ```
+   wget ...
+   ```
+10. Run the script, passing in the options
+   ```
+   sudo python3 miner.py <ethermine|flexpool> <wallet>
+   ```
+11. The script should run and you should see the display light up and then start displaying the base data and a countdown.
+12. If everything is working, press ctrl-c to stop the process.
+13. Edit the rc.local to get the script to run on bootup.
+   ```
+   sudo nano /etc/rc.local
+   ```
+14. Add the following line above the "exit 0" line
+   ```
+   sudo python3 ~pi/miner.py > ~pi/miner.log &
+   ```
+15. Press ctrl-x, select yes to save, and exit the file
+16. Reboot the pi to make sure this worked correctly
+   ```
+   sudo shutdown -r now
+   ```
---
+++ miner.py
@@ -0,0 +1,165 @@
+# -*- coding: utf-8 -*-
+import sys
+import time
+import json
+import requests
+
+#Import Blinka
+import digitalio
+import board
+# Import Python Imaging Library
+from PIL import Image, ImageDraw, ImageFont
+import adafruit_rgb_display.st7789 as st7789
+
+def get_ethermine_values(wallet):
+    api_url = "https://api.ethermine.org/miner/{}/dashboard".format(wallet)
+    r = requests.get(em_api_url)
+    data = json.loads(r.text)
+    ret_value = {}
+    ret_value["unpaid"] = data["data"]["currentStatistics"]["unpaid"]/1000000000000000000
+    ret_value["workers"] = data["data"]["currentStatistics"]["activeWorkers"]
+    ret_value["reported_hashrate"] = data["data"]["currentStatistics"]["reportedHashrate"]/1000000
+    ret_value["actual_hashrate"] = data["data"]["currentStatistics"]["currentHashrate"]/1000000
+    ret_value["invalid_shares"] = data["data"]["currentStatistics"]["invalidShares"]
+    ret_value["stale_shares"] = data["data"]["currentStatistics"]["staleShares"]
+    return ret_value
+
+def get_flexpool_values(wallet):
+    return {}
+
+if len(sys.argv) != 3 or (sys.argv[1] != "ethermine" and sys.argv[1] != "flexpool"):
+    print("Usage: sudo python3 miner.py ethermine|flexpool wallet")
+    exit
+
+pool = sys.argv[1]
+wallet = sys.argv[2]
+if wallet.startswith("0x"):
+    wallet = wallet[2:]
+
+vals = get_ethermine_values(wallet)
+print(vals)
+exit
+
+value_api_url = "https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=usd"
+
+# Configuration for CS and DC pins (these are FeatherWing defaults on M0/M4):
+cs_pin = digitalio.DigitalInOut(board.CE0)
+dc_pin = digitalio.DigitalInOut(board.D25)
+reset_pin = None
+
+# Config for display baudrate (default max is 24mhz):
+BAUDRATE = 64000000
+
+# Setup SPI bus using hardware SPI:
+spi = board.SPI()
+
+# Create the ST7789 display:
+disp = st7789.ST7789(spi, cs=cs_pin, dc=dc_pin, rst=reset_pin, baudrate=BAUDRATE,
+                     width=135, height=240, x_offset=53, y_offset=40)
+
+# Create blank image for drawing.
+# Make sure to create image with mode 'RGB' for full color.
+height = disp.width   # we swap height/width to rotate it to landscape!
+width = disp.height
+image = Image.new('RGB', (width, height))
+rotation = 90
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0, 0, width, height), outline=0, fill=(0, 0, 0))
+disp.image(image, rotation)
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+# Alternatively load a TTF font.  Make sure the .ttf font file is in the
+# same directory as the python script!
+# Some other nice fonts to try: http://www.dafont.com/bitmap.php
+font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 24)
+big_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 48)
+small_font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 18)
+
+font_height = font.getsize("a")[1]
+big_font_height = big_font.getsize("a")[1]
+
+red = "#FF0000"
+green = "#00FF00"
+blue = "#0000FF"
+yellow = "#FFFF00"
+
+# Turn on the backlight
+backlight = digitalio.DigitalInOut(board.D22)
+backlight.switch_to_output()
+backlight.value = True
+
+# Add buttons as inputs
+buttonA = digitalio.DigitalInOut(board.D23)
+buttonA.switch_to_input()
+
+duration = 300
+counter = duration
+
+state = 0
+while True:
+    # Draw a black filled box to clear the image.
+    draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+    if counter == duration:
+        try:
+            miner_data = get_ethermine_values(wallet) if pool == "ethermine" else get_flexpool_values(wallet)
+            r = requests.get(value_api_url)
+            data = json.loads(r.text)
+            eth_value = data["ethereum"]["usd"]
+        except:
+            y = top
+            draw.text((x,y),"ERROR LOADING DATA", font=font, fill=red)
+            print("error")
+            time.sleep(60)
+            continue
+
+    y = top
+
+    if not buttonA.value:
+        state = 1 if state == 0 else 0
+
+    if state == 0:
+        draw.text((x,y),"{:.5f}".format(miner_data["unpaid"]),font=big_font, fill=green if miner_data["workers"] >= 2 and miner_data["invalid_shares"] < 3 else red)
+        y += big_font_height
+        draw.text((x,y),"${:.2f}".format(miner_data["unpaid"]*eth_value), font=big_font, fill=green)
+        y += big_font_height
+        draw.text((x,y),"${:.2f}".format(eth_value), font=big_font, fill=green)
+        draw.text((width-40,0), str(counter), font=small_font, fill=yellow)
+    else:
+        draw.text((x,y),"Unpaid: ", font=font, fill=blue)
+        draw.text((x+font.getsize("Unpaid: ")[0],y), "{:.5f}".format(miner_data["unpaid"]), font=font, fill=green)
+        y += font_height
+        draw.text((x,y),"Value: ", font=font, fill=blue)
+        draw.text((x+font.getsize("Value: ")[0],y+5), "${:.2f} ${:.2f}".format(eth_value*miner_data["unpaid"], eth_value), font=small_font, fill=green)
+        y += font_height
+        draw.text((x,y),"Workers: Mh/s (R/A):", font=font, fill=blue)
+        y += font_height
+        draw.text((x,y), "{}: {:.1f} / ".format(miner_data["workers"], miner_data["reported_hashrate"]), font=font, fill=green)
+        draw.text((x+font.getsize("{} - {:.1f} / ".format(miner_data["workers"], miner_data["reported_hashrate"]))[0],y), "{:.1f}".format(miner_data["actual_hashrate"]), font=font, fill=green if miner_data["actual_hashrate"] >= miner_data["reported_hashrate"] else yellow)
+        y += font_height
+        draw.text((x,y),"Stale / Invalid: ", font=font, fill=blue)
+        draw.text((x+font.getsize("Stale / Invalid: ")[0],y), str(miner_data["stale_shares"]), font=font, fill=green)
+        draw.text((x+font.getsize("Stale / Invalid: {}".format(miner_data["stale_shares"]))[0],y), " / ", font=font, fill=blue)
+        draw.text((x+font.getsize("Stale / Invalid: {} / ".format(miner_data["stale_shares"]))[0],y),str(miner_data["invalid_shares"]), font=font, fill=green if miner_data["invalid_shares"] == 0 else red)
+        y += font_height
+        draw.text((x,y), str(counter), font=small_font, fill=yellow)
+
+    # Display image.
+    disp.image(image, rotation)
+    counter -= 1
+    
+    if counter == 0:
+        counter = duration
+    time.sleep(1)
+
+
+
